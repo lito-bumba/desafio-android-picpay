5d619eb6bbcf808c518347280b462958f01fbe75
Lito Bumba <litocdbumba@gmail.com>
2025-03-06 21:36:53 -0300

    Rodando o app

--- app/src/main/java/com/picpay/desafio/android/User.kt
+++ app/src/main/java/com/picpay/desafio/android/User.kt
@@ -1,13 +1,18 @@
 package com.picpay.desafio.android
 
+import android.annotation.SuppressLint
+import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+@SuppressLint("ParcelCreator")
 data class User(
     @SerializedName("img") val img: String,
     @SerializedName("name") val name: String,
     @SerializedName("id") val id: Int,
     @SerializedName("username") val username: String
-) : Parcelable
+) : Parcelable {
+    override fun describeContents(): Int = Parcelable.CONTENTS_FILE_DESCRIPTOR
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = Unit
+}
--- app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
+++ app/src/main/java/com/picpay/desafio/android/UserListItemViewHolder.kt
@@ -1,29 +1,36 @@
 package com.picpay.desafio.android
 
 import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.list_item_user.view.*
+import de.hdodenhof.circleimageview.CircleImageView
 
 class UserListItemViewHolder(
     itemView: View
 ) : RecyclerView.ViewHolder(itemView) {
 
+    private val name = itemView.findViewById<TextView>(R.id.name)
+    private val username = itemView.findViewById<TextView>(R.id.username)
+    private val progressBar = itemView.findViewById<ProgressBar>(R.id.progressBar)
+    private val picture = itemView.findViewById<CircleImageView>(R.id.picture)
+
     fun bind(user: User) {
-        itemView.name.text = user.name
-        itemView.username.text = user.username
-        itemView.progressBar.visibility = View.VISIBLE
+        name.text = user.name
+        username.text = user.username
+        progressBar.visibility = View.VISIBLE
         Picasso.get()
             .load(user.img)
             .error(R.drawable.ic_round_account_circle)
-            .into(itemView.picture, object : Callback {
+            .into(picture, object : Callback {
                 override fun onSuccess() {
-                    itemView.progressBar.visibility = View.GONE
+                    progressBar.visibility = View.GONE
                 }
 
                 override fun onError(e: Exception?) {
-                    itemView.progressBar.visibility = View.GONE
+                    progressBar.visibility = View.GONE
                 }
             })
     }
